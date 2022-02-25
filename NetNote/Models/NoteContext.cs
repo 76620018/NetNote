35a9c1a82287c6deb3e5a0fa929894a145e49b0c
wen <76620018@qq.com>
2022-02-25 18:26:34 +0800

    添加项目文件。

---
+++ NetNote/Models/NoteContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetNote.Models
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) : base(options)
+        {
+
+        }
+
+        //public static string connectiongString { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //        optionsBuilder.UseSqlServer(connectiongString);
+        //}
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<NoteType> NoteTypes { get; set; }
+    }
+}
---
+++ NetNote/Repository/INoteRepository.cs
@@ -0,0 +1,16 @@
+﻿using NetNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetNote.Repository
+{
+    public interface INoteRepository
+    {
+        Task<Note> GetByIdAsync(int id);
+        Task<List<Note>> ListAsync();
+        Task AddAsync(Note note);
+        Task UpdateAsync(Note note);
+    }
+}
