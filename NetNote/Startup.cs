f40e8bb9c057d981aa8a11a4eb390d5d4c48f3ef
wen <76620018@qq.com>
2022-03-01 11:57:40 +0800

    添加InitData"

--- NetNote/Startup.cs
+++ NetNote/Startup.cs
@@ -50,6 +50,7 @@ namespace NetNote
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            InitData(app.ApplicationServices);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -81,6 +82,32 @@ namespace NetNote
                     name: "default",
                     pattern: "{controller=Note}/{action=Index}/{id?}");
             });
+        }
+        private void InitData(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                //获取注入的NoteContext
+                var db = serviceScope.ServiceProvider.GetRequiredService<NoteContext>();
+                db.Database.EnsureCreated();//如果数据库不存在则创建，存在则不做操作
+                if (!db.NoteTypes.Any())//不存在类型则添加
+                {
+                    var notetypes = new List<NoteType>{
+                        new NoteType{ Name="日常记录"},
+                        new NoteType{ Name="代码收藏"},
+                        new NoteType{ Name="消费记录"},
+                        new NoteType{ Name="网站收藏"}
+                    };
+                    db.NoteTypes.AddRange(notetypes);
+                    db.SaveChanges();
+                }
+                if (!db.Users.Any()) //不存在用户，默认添加admin用户
+                {
+                    var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<NoteUser>>();
+                    var noteUser = new NoteUser { UserName = "admin", Email = "admin@qq.com" };
+                    userManager.CreateAsync(noteUser, "a123456").Wait();
+                }
+            }
         }
     }
 }
